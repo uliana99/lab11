3c667f4049d8e71b1d9a4b700e8c20b4cc9b53b4
uliana99 <koshkinauliana99@gmail.com>
2017-11-07 00:40:13 +0300

    Update demo.cpp

--- sources/demo.cpp
+++ sources/demo.cpp
@@ -17,19 +17,19 @@ try {
     variables_map vm;
     store(parse_command_line(argc, argv, desc), vm);
 
-    std::string gAddress = getenv("HOME");
-    gAddress += "/.config/demo.cfg";
+    std::string path = getenv("HOME");
+    path += "/.config/demo.cfg";
 
-    if(exists(gAddress))
-    	store(parse_config_file<char>( gAddress.c_str(), desc ), vm);
+    if(exists(path))
+    	store(parse_config_file<char>( path.c_str(), desc ), vm);
 
     notify(vm);
 
     if(vm.count("help")) {
       std::cout << desc << '\n';
     } else if(getenv("DEMO_OUTPUT") != nullptr) {
-        std::string _trAddress = getenv("DEMO_OUTPUT");
-        std::ofstream out(_trAddress, std::ios_base::app);
+        std::string _path = getenv("DEMO_OUTPUT");
+        std::ofstream out(_path, std::ios_base::app);
         while(std::cin >> text) {
 			print(text, out);
 			out << std::endl;
